37e5e8616862fa8480db7315bee7e77e283f476d
colenelson33 <colenelson1210@gmail.com>
2022-02-15 13:45:31 -0600

    Setting up test

---
+++ Learning/Learning/LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningApp.swift
+//  Learning
+//
+//  Created by 90304588 on 2/15/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
